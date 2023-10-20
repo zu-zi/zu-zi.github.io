4ab94a2870d864929466d867479c986ef14b2dfb
zu-zi <735055854@qq.com>
2023-10-20 20:36:09 +0800

    my website

---
+++ task1/P3 index(附件).js
@@ -0,0 +1,33 @@
+const array = [
+  {
+    "username": "zu-zi",
+    "remark": "发疯发疯，反正之后可以删掉\n",
+    "time": "2023/10/10 下午3:24:54"
+  },
+  {
+    "username": "zu-zi",
+    "remark": "多写一点，多水一点回复，不然好冷清喔",
+    "time": "2023/10/10 下午3:24:25"
+  },
+  {
+    "username": "zu-zi",
+    "remark": "需要学习的知识都学的差不多啦，开始正式写属于我的blog哩✌✌✌",
+    "time": "2023/10/5 下午8：39：58"
+  },
+  {
+    "username": "zu-zi",
+    "remark": "测试一下",
+    "time": "2023/10/4 下午9：36：08"
+  },
+  {
+    "username": "zu-zi",
+    "remark": "就是最棒滴捏",
+    "time": "2023/10/4 下午8：35：18"
+  },
+  {
+    "username": "zu-zi",
+    "remark": "我终于运行成功啦 耶耶✌",
+    "time": "2023/10/4 下午8：34：43"
+  }
+]
+localStorage.setItem(`comment of zu-zi's space`)
