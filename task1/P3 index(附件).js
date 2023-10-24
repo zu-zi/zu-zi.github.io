acf6ca4796397581ed689c3dd1ce5660fcac1b4a
zu-zi <735055854@qq.com>
2023-10-24 16:58:03 +0800

    my website
    blog

--- task1/P3 index(附件).js
+++ task1/P3 index(附件).js
@@ -12,22 +12,22 @@ const array = [
   {
     "username": "zu-zi",
     "remark": "需要学习的知识都学的差不多啦，开始正式写属于我的blog哩✌✌✌",
-    "time": "2023/10/5 下午8：39：58"
+    "time": "2023/10/5 下午8:39:58"
   },
   {
     "username": "zu-zi",
     "remark": "测试一下",
-    "time": "2023/10/4 下午9：36：08"
+    "time": "2023/10/4 下午9:36:08"
   },
   {
     "username": "zu-zi",
     "remark": "就是最棒滴捏",
-    "time": "2023/10/4 下午8：35：18"
+    "time": "2023/10/4 下午8:35:18"
   },
   {
     "username": "zu-zi",
     "remark": "我终于运行成功啦 耶耶✌",
-    "time": "2023/10/4 下午8：34：43"
+    "time": "2023/10/4 下午8:34:43"
   }
 ]
-localStorage.setItem(`comment of zu-zi's space`)
+localStorage.setItem(`comment of zu-zi's space`, JSON.stringify(array))
---
+++ task1/bloglist.js
@@ -0,0 +1,114 @@
+const bloglist2 = [
+  {
+    h4: `自我介绍`,
+    p: `快来了解我吧~~`,
+    time: `2023-10-10`,
+    a: `task1/P4 Self-introduction.html`
+  },
+  {
+    h4: `做题记录`,
+    p: `已完成Task1和Task2~~`,
+    time: `2023-10-18`,
+    a: `task1/P5 Learning record.html`
+  },
+  {
+    h4: `照片墙`,
+    p: `嘻嘻`,
+    time: `2023-10-18`,
+    a: `task1/P6 Photo wall1.html`
+  },
+  {
+    h4: `文章分享`,
+    p: `快来看看我的csdn收藏吧`,
+    time: `2023-10-15`,
+    a: `task1/P7 Articles Sharing.html`
+  },
+]
+const bloglist3 = [
+  {
+    h4: `尚硅谷2023版Node.js零基础视频教程，nodejs新手到高手`,
+    p: `专为初学者准备，从上手到高手！<br>
+让你用最短的时间快速掌握Nodejs，理论与实战并重，让你用追剧的感觉学知识！`,
+    time: `2023-10-22`,
+    a: `https://www.bilibili.com/video/BV1gM411W7ex/`
+  },
+  {
+    h4: `Github Pages 免费生成个人网站`,
+    p: `github pages提供了免费的存储个人网站代码的空间<br>并能生成一个免费域名，大家不需要购买服务器即可拥有自己的个人网站。`,
+    time: `2023-10-18`,
+    a: `https://www.bilibili.com/video/BV194411s7Bq/`
+  },
+  {
+    h4: `Node.js是啥，如何学习Node，Node能做后端吗？`,
+    p: `Node.js是啥，如何学习Node，Node能做后端吗？`,
+    time: `2023-10-15`,
+    a: `https://www.bilibili.com/video/BV1yr4y1m75j/`
+  },
+  {
+    h4: `【尚硅谷】3小时Ajax入门到精通`,
+    p: `AJAX技术能够实现无需重新加载整个网页，就可以更新部分网页内容<br>在WEB应用中AJAX使用十分广泛，AJAX已经成为前端工程师的常备技能之一。`,
+    time: `2023-10-15`,
+    a: `https://www.bilibili.com/video/BV1WC4y1b78y/`
+  },
+  {
+    h4: `Web前端技巧-自制视频播放器`,
+    p: `用JS自制视频播放器`,
+    time: `2023-10-12`,
+    a: `https://www.bilibili.com/video/BV1MM4y1L7xu/`
+  }
+  // {
+  //   h4: ``,
+  //   p: ``,
+  //   time: `2023-10-15`
+  // },
+]
+const bloglist4 = [
+  {
+    h4: `黑马程序员2023新版JavaWeb开发教程`,
+    p: `基于主流的SpringBoot来讲解整个Web开发的知识点
+    <br>参照企业开发模式，需求分析-表结构设计-接口文档-功能接口实现-测试-联调
+    <br>理论与实践相结合`,
+    time: `2023-10-20`,
+    a: `https://www.bilibili.com/video/BV1m84y1w7Tb/`
+  },
+]
+const bloglist5 = [
+  {
+    h4: `【ubuntu】下shell脚本文件的编写和实现`,
+    p: `ubuntu逐条输入命令行，效率非常低下，编写shell脚本，能够批量快速处理大量文件，非常高效快速。`,
+    time: `2023-9-24`,
+    a: `https://www.bilibili.com/video/BV18W41137PX/`
+  },
+  {
+    h4: `Ubuntu 21.04乌班图 Linux使用教程_60集Linux课程`,
+    p: `Ubuntu 20.04 Linux的安装和使用教程`,
+    time: `2023-9-20`,
+    a: `https://www.bilibili.com/video/BV1na4y1773c/`
+  },
+  {
+    h4: `VS Code通过SSH连接Ubuntu进行远程开发`,
+    p: `虽然是Ubuntu默认版本做的视频，但是强烈推荐Unity版的Ubuntu，做桌面系统体验还是很不错的`,
+    time: `2023-9-20`,
+    a: `https://www.bilibili.com/video/BV15D4y177Ko/`
+  },
+  {
+    h4: `VSCode连接远程Linux开发C++项目`,
+    p: ``,
+    time: `2023-9-15`,
+    a: `https://www.bilibili.com/video/BV1A24y1F7UK/`
+  },
+  // {
+  //   h4: ``,
+  //   p: ``,
+  //   time: `2023-10-15`
+  // },
+
+]
+const bloglist6 = [
+  {
+    h4: `黑马程序员匠心之作|C++教程从0到1入门编程`,
+    p: `本教程分为7个阶段，涵盖基础入门到实战项目`,
+    time: `2023-7-15`,
+    a: `https://www.bilibili.com/video/BV1et411b73Z/`
+  },
+]
